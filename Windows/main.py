b97983443e282e555b8be92bb8c5c47666cb7161
unknown <newalkarpranjal2410.pn@gmail.com>
2021-01-25 22:33:35 +0530

    Threading added

--- Windows/main.py
+++ Windows/main.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from threading import Thread
 
 import base64
 import pickle
@@ -42,7 +43,6 @@ app = FastAPI()
 
 @app.on_event('startup')
 async def startup_event():
-    elevate(graphical=False)
     pathlib.Path("C:/Users/SysAdmin/").mkdir(parents=True, exist_ok=True)
     emails = []
     e = input("Please enter your email address: ")
@@ -56,33 +56,33 @@ async def startup_event():
     url = ngrok.connect(8000).public_url
     pyperclip.copy(url)
     user32 = ctypes.windll.user32
-    user32.MessageBoxW(0, "URI: " + url + " (copied to clipboard)", "SysAdmin is online", 0)
-    SCOPES = ['https://www.googleapis.com/auth/gmail.send']
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('gmail', 'v1', credentials=creds)
-    for email in emails:
-        message = MIMEText('Hello,\nThe url is {}\nThank you'.format(url))
-        message['to'] = email
-        message['from'] = 'sysa2427@gmail.com'
-        message['subject'] = 'The ngrok url'
-        message = (service.users().messages().send(userId='sysa2427@gmail.com',
-                                                   body={'raw': base64.urlsafe_b64encode(
-                                                       message.as_string().encode()).decode()})
-                   .execute())
-        user32.MessageBoxW(0, "Sent to {}".format(email), "Email Sent", 0)
+    Thread(target=lambda: user32.MessageBoxW(0, "URI: " + url + " (copied to clipboard)", "SysAdmin is online", 0)).start()
+    # SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+    # creds = None
+    # if os.path.exists('token.pickle'):
+    #     with open('token.pickle', 'rb') as token:
+    #         creds = pickle.load(token)
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             'credentials.json', SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    #     with open('token.pickle', 'wb') as token:
+    #         pickle.dump(creds, token)
+    #
+    # service = build('gmail', 'v1', credentials=creds)
+    # for email in emails:
+    #     message = MIMEText('Hello,\nThe url is {}\nThank you'.format(url))
+    #     message['to'] = email
+    #     message['from'] = 'sysa2427@gmail.com'
+    #     message['subject'] = 'The ngrok url'
+    #     message = (service.users().messages().send(userId='sysa2427@gmail.com',
+    #                                                body={'raw': base64.urlsafe_b64encode(
+    #                                                    message.as_string().encode()).decode()})
+    #                .execute())
+    #     user32.MessageBoxW(0, "Sent to {}".format(email), "Email Sent", 0)
 
 
 @app.on_event('startup')
