8e59e1f2a34cd9f0a9f83ff46500ff20739ca68d
tanmaypardeshi <tanmaypardeshi@gmail.com>
2021-01-25 15:28:21 +0530

    Added psutil docs with _asdict()

--- Ubuntu/scripts/psutil_script.py
+++ Ubuntu/scripts/psutil_script.py
@@ -3,6 +3,9 @@ import psutil
 
 def psutil_controller(req):
     try:
-        return getattr(psutil, req.func)(**req.dargs if isinstance(req.dargs, dict) else {})._asdict()
+        try:
+            return getattr(psutil, req.func)(**req.dargs if isinstance(req.dargs, dict) else {})._asdict()
+        except:
+            return getattr(psutil, req.func)(**req.dargs if isinstance(req.dargs, dict) else {})
     except Exception as e:
         return str(e)
